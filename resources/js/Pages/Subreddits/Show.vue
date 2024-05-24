c0c98a7d35bc94c5b5810546dc1c190d8960f0fb
smugytea4real <thomasandromeda999@gmail.com>
2024-05-25 04:36:00 +0700

    Show the subreddit page

---
+++ resources/js/Pages/Subreddits/Show.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+defineProps({
+    subreddit: Object(),
+})
+
+</script>
+<template>
+    <h1>Subreddit {{ subreddit.name }} show page</h1>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Backend\CommunityController;
+use App\Http\Controllers\Frontend\SubredditController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -21,12 +22,14 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/r/{slug}', [SubredditController::class, 'show'])->name('subreddit.show');
+
 Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('/dashboard', function () {
-        return Inertia::render('Communities/Index');
+        return Inertia::render('Dashboard');
     })->name('dashboard');
 
-    Route::resource('communities', CommunityController::class);
+    Route::resource('/dashboard/communities', CommunityController::class);
 });
 
 require __DIR__ . '/auth.php';
