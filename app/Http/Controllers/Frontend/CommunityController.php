b6d13f6f17d7649f14e5a19e46b680fce656358a
smugytea4real <thomasandromeda999@gmail.com>
2024-05-26 00:15:59 +0700

    Display post with pagination

--- app/Http/Controllers/Frontend/CommunityController.php
+++ app/Http/Controllers/Frontend/CommunityController.php
@@ -13,8 +13,8 @@ class CommunityController extends Controller
     public function show($slug)
     {
         $community = Community::where('slug', $slug)->first();
-        $posts = CommunityPostResource::collection($community->posts()->paginate(12));
-        
+        $posts = CommunityPostResource::collection($community->posts()->paginate(3));
+
         return Inertia::render('Frontend/Communities/Show', compact('community', 'posts'));
     }
 }
--- resources/js/Pages/Frontend/Communities/Show.vue
+++ resources/js/Pages/Frontend/Communities/Show.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Guestlayout from '@/Layouts/GuestLayout.vue';
 import { Link } from '@inertiajs/vue3';
+import PostCard from '@/Components/PostCard.vue';
 
 defineProps({
-    community: Object(),
+    community: Object,
+    posts: Object,
 })
 
 </script>
@@ -13,14 +15,20 @@ defineProps({
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 r/{{ community.name }}
             </h2>
+            <div class="flex justify-end m-2 p-2">
+                <Link
+                  v-if="$page.props.auth.auth_check"
+                  :href="route('communities.posts.create', community.slug)"
+                  class="px-3 py-2 rounded bg-indigo-500 hover:bg-indigo-300 text-white"
+                  >Create Post</Link>
+            </div>
         </template>
-        <div class="flex justify-end m-2 p-2">
-            <Link
-              v-if="$page.props.auth.auth_check"
-              :href="route('communities.posts.create', community.slug)"
-              class="px-3 py-2 rounded bg-indigo-500 hover:bg-indigo-300 text-white"
-              >Create Post</Link>
-        </div>
-        <h1>Community {{ community.name }} show page</h1>
+        <section class="m-2 p-2">
+            <PostCard 
+            v-for="post in posts.data" 
+            :key="post.id"
+            :post="post"
+            :community="community.name"/>
+        </section>
     </Guestlayout>
 </template>
