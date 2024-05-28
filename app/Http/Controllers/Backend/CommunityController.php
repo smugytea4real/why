0c4c972e72a9b4fb97775a653ac7494f970ccd12
smugytea4real <thomasandromeda999@gmail.com>
2024-05-28 11:24:16 +0700

    added authorization

--- app/Http/Controllers/Backend/CommunityController.php
+++ app/Http/Controllers/Backend/CommunityController.php
@@ -61,6 +61,7 @@ class CommunityController extends Controller
      */
     public function edit(Community $community)
     {
+        $this->authorize('update', $community);
         return Inertia::render('Communities/Edit', [
             'community' => $community
         ]);
@@ -71,6 +72,7 @@ class CommunityController extends Controller
      */
     public function update(CommunityStoreRequest $request, Community $community)
     {
+        $this->authorize('update', $community);
         $community->update($request->validated());
 
         return to_route('communities.index')->with('message', 'Community updated successfully');
@@ -81,6 +83,7 @@ class CommunityController extends Controller
      */
     public function destroy(Community $community)
     {
+        $this->authorize('delete', $community);
         $community->delete();
         return back()->with('message', 'Community deleted successfully');
     }
